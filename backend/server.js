53d3974bacfa8f71e765ce785d903fedc2c23523
sagar yadav <padloab@gmail.com>
2026-03-21 15:51:19 +0530

    Mongodb conected

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,11 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB Connected ✅"))
+.catch(err => console.log(err));
 
 app.use(cors({
   origin: "http://localhost:5173"
